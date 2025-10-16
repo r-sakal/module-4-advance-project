1feff0bf7ed79b5d2f96e0011cde7ed94a34fef1
Rob <rob.sakal23@gmail.com>
2025-10-16 02:18:27 -0400

    background shapes js done

--- index.js
+++ index.js
@@ -3,6 +3,21 @@
 // service id service_ppzsc4k
 let isModalOpen = false;
 let contrastToggle = false;
+const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape")
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+
+    for (let i = 0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+    }
+
+
+}
 
 function toggleContrast() {
     contrastToggle = !contrastToggle;
